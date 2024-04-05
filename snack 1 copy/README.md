f29daa52646b530938557e77a7a587704b41d61a
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-05 11:38:37 +0200

    feat: first implementation

---
+++ snack 1 copy/README.md
@@ -0,0 +1,6 @@
+# istruzioni
+Esercizio 1:
+Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+Ogni volta che ne crei uno, stampalo a schermo.
+
---
+++ snack 1 copy/js/script.js
@@ -0,0 +1,21 @@
+// INPUT
+const userNumb = parseInt(prompt("inserisci un numero"));
+console.log(userNumb);
+/**
+ * genera un numero di array uguale a input utente contenenti 10 numeri fra i e 100
+ * @param {number} genArray input utente
+ * @returns {array}
+ */
+function arrayGen(genArray) {
+    for (let i = 0; i <= userNumb; i++) {
+        const emptyArray = [];
+        console.log(emptyArray);
+        for (let j = 0; j <= 10; j++) {
+            emptyArray.push = Math.floor(Math.random() * 100) + 1;
+            console.log(emptyArray);
+        }
+    }
+    return emptyArray
+}
+
+arrayGen(userNumb)
